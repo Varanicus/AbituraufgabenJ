e9899814e1e46396bf796ae140b3fcf5a2c846a6
FlorianBoldt <FlorianBoldt@0-208-11>
2022-10-04 14:28:35 +0200

    first commit

---
+++ Abiaufgabe1/src/ListenKnoten.java
@@ -0,0 +1,10 @@
+
+public class ListenKnoten<Integer>{
+
+	public Integer daten;
+	public ListenKnoten<Integer> nachfolger;
+	
+	public ListenKnoten(Integer daten, ListenKnoten<Integer> nachfolger) {
+		
+	}
+}
