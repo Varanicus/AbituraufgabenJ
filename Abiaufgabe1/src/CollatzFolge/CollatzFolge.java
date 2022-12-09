2e43b6919d02ed72d67411f052e4a1d596ea4d0b
FlorianBoldt <FlorianBoldt@0-209-12>
2022-12-09 09:07:48 +0100

    cc

--- Abiaufgabe1/src/CollatzFolge/CollatzFolge.java
+++ Abiaufgabe1/src/CollatzFolge/CollatzFolge.java
@@ -1,32 +1,24 @@
 package CollatzFolge;
 
 public class CollatzFolge {
-    public static void generate(int n) {
-        System.out.println(n);
+	public static void generate(int n) {
+		System.out.println(n);
 
+		if (n == 1) {
+			return;
+		}
 
+		if (n % 2 == 0) {
+			generate(n / 2);
+		}
 
-       // If n is 1, we're done
-        if (n == 1) {
-            return;
-        }
+		else {
+			generate(3 * n + 1);
+		}
+	}
 
+	public static void main(String[] args) {
 
-
-       // If n is even, divide it by 2
-        if (n % 2 == 0) {
-            generate(n / 2);
-        }
-        // If n is odd, multiply it by 3 and add 1
-        else {
-            generate(3 * n + 1);
-        }
-    }
-
-
-
-   public static void main(String[] args) {
-        // Start with the number n = 1
-        generate(100);
-    }
+		generate(100);
+	}
 }
